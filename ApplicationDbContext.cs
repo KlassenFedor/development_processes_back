79b1d8179e6d2a2e0d45315541615672bad30b79
Klassen Fedor <fyodor.klassen@yandex.ru>
2023-03-04 23:43:58 +0700

    Db added

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dev_processes_backend
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
